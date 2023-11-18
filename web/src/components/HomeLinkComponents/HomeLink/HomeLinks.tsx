38067022b770a3af2954e16b1d27366209719743
Victoria McCrady <vmccrady@expediagroup.com>
2023-11-17 16:48:38 -0800

    style: remove test border, add custom breakpoint for re-sizing text,
    custom breakpoint was needed because lg screens below 1054px flexwrap 9xl font. This looks more
    responsive

--- web/src/components/HomeLinkComponents/HomeLink/HomeLinks.tsx
+++ web/src/components/HomeLinkComponents/HomeLink/HomeLinks.tsx
@@ -2,7 +2,7 @@ import { Link, routes } from '@redwoodjs/router'
 
 const HomeLinks = () => {
   return (
-    <div className="m-2 flex basis-2/3 flex-col items-center text-5xl italic text-zinc-50 sm:text-6xl md:text-8xl lg:text-9xl">
+    <div className="m-2 flex basis-2/3 flex-col items-center text-5xl italic text-zinc-50 sm:text-6xl md:text-8xl min-[1154px]:text-9xl">
       <Link
         className="m-5 p-4 transition delay-100 hover:-translate-y-1 hover:scale-110 hover:text-black hover:underline motion-reduce:transition-none motion-reduce:hover:transform-none"
         to={routes.portfolio()}
--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -10,7 +10,7 @@ const HomePage = () => {
       <RouteAnnouncement visuallyHidden>
         Home page with links to the rest of my portfolio. Thanks for visiting!
       </RouteAnnouncement>
-      <main className="m-2 flex w-screen basis-full flex-col items-center justify-around border-2 border-red-400 tracking-wide md:flex-row md:flex-wrap">
+      <main className="m-2 flex w-screen basis-full flex-col items-center justify-around tracking-wide md:flex-row md:flex-wrap">
         <div className="m-5 flex flex-initial basis-1/3 flex-col items-center justify-center rounded-sm border-8 border-zinc-50 px-5 py-10 text-4xl font-bold text-zinc-50 md:text-6xl lg:text-7xl">
           <h1>Victoria</h1>
           <h2>Briar</h2>
