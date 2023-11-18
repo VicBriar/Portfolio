3d7d9828ddf6710e4c9f4836b6e6ba826f08c3dd
Victoria McCrady <vmccrady@expediagroup.com>
2023-11-17 16:40:20 -0800

    create tests

--- web/src/components/HomeLinkComponents/HomeLink/HomeLinks.test.tsx
+++ web/src/components/HomeLinkComponents/HomeLink/HomeLinks.test.tsx
@@ -1,3 +1,4 @@
+import { routes } from '@redwoodjs/router'
 import { render } from '@redwoodjs/testing/web'
 
 import HomeLinks from './HomeLinks'
@@ -10,5 +11,24 @@ describe('HomeLinks', () => {
     expect(() => {
       render(<HomeLinks />)
     }).not.toThrow()
+  })
+  it('renders all three links', () => {
+    const tree = render(<HomeLinks />)
+    const portfolioLink = tree.getByText('Portfolio')
+    const experienceLink = tree.getByText('Experience')
+    const aboutMeLink = tree.getByText('About Me')
+
+    expect(portfolioLink).toBeInTheDocument()
+    expect(portfolioLink.closest('a')).toHaveAttribute(
+      'href',
+      routes.portfolio()
+    )
+    expect(experienceLink).toBeInTheDocument()
+    expect(experienceLink.closest('a')).toHaveAttribute(
+      'href',
+      routes.experience()
+    )
+    expect(aboutMeLink).toBeInTheDocument()
+    expect(aboutMeLink.closest('a')).toHaveAttribute('href', routes.bio())
   })
 })
